c743e754883dda520cb0aaf0c3545becfc4e9a82
tonylumia710 <tonylumia710@hotmail.com>
2021-12-15 20:50:09 -0300

    carga funcional

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+app.use(express.static('public'));
+
+app.listen(port, () => console.log('Servidor corriendo por el puerto '+ port));
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+
